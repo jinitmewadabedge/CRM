92b047c9ac1711c7937f10950e48ee8af693a9db
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-09-10 19:34:00 +0530

    Final Commit

--- server/hashPassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const plainPassword = "naman@123";
-
-bcrypt.hash(plainPassword, 10, (err, hash) => {
-    if(err) throw err;
-    console.log("Hashed Password", hash);
-})
