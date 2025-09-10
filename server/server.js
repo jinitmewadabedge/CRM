b7053521e71a4b3a4c58af38d9ef5433e2f70878
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-09-11 01:17:20 +0530

    Cors Issue Solved

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
