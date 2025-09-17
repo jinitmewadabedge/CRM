b2330f7bbba63861681c980cab5ef05c485ccc90
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-09-18 00:06:23 +0530

    Cors issuse solved

--- server/server.js
+++ server/server.js
@@ -12,13 +12,24 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://bedge-crm.vercel.app"
+]
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: "https://bedge-crm.vercel.app",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
 app.options("*", cors());
 app.use('/api/auth', authRoutes);
