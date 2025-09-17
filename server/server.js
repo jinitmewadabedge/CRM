84dcc5e33439b58199833658468d0db57b56f607
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-09-17 23:58:47 +0530

    Cors issues solved

--- server/server.js
+++ server/server.js
@@ -16,10 +16,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "https://bedge-crm.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
 }));
+app.options("*", cors());
 app.use('/api/auth', authRoutes);
 app.use('/', testRoute);
 app.use('/api/leads', leadRoute);
