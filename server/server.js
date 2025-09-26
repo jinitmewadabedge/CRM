090e4ee5cf2b178d9d1204377581b871e173f7ff
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-09-27 02:03:18 +0530

    cors issues

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://bedge-crm.vercel.app"
+    "https://bedge-crm.vercel.app",
+    "https://bedge-crm-backend.vercel.app/api/auth/login"
 ];
 
 app.use(cors({
