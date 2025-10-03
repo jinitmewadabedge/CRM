3d32dc1054bdb138a54cfa85d42b05d883dcf1b9
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-10-04 01:40:45 +0530

    vite config js changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/",
+  server:{
+    proxy:{
+      '/api': {
+        target: process.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
