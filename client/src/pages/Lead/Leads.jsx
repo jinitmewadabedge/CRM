a99840149b2e11f6957095c2a30eee7caadb5821
jinitmewadabedge <jinitmewadabedge@gmail.com>
2025-11-12 03:26:45 +0530

    Modal Changed second time

--- client/src/pages/Lead/Leads.jsx
+++ client/src/pages/Lead/Leads.jsx
@@ -910,18 +910,16 @@ const Leads = () => {
         source: "",
         status: ""
       })
-      // fetchUnassignedLeads();
+
       fetchBackendLeads();
 
-      const modalElement = document.getElementById("addNewLead");
-      const modalInstance = bootstrap.Modal.getInstance(modalElement);
-      if(modalInstance){
-        modalElement.hide();
-      } else {
-        const newModal = new bootstrap.Modal(modalElement);
-        newModal.hide();
-      }
-
+      setTimeout(() => {
+        const modalEl = document.getElementById("addNewLead");
+        if (modalEl) {
+          const modalInstance = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
+          modalInstance.hide();
+        }
+      }, 300);
 
     } catch (error) {
       console.error("Error adding lead:", error.response?.data || error.message);
